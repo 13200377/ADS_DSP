38ad019115ccd8807a8927bac21aa8cdbfd2f58e
Hayden Keers <13200377@student.uts.edu.au>
2021-10-24 21:20:29 +1100

    Fixed compile errors for deserializer and PISO

--- src/PISO_arr.vhd
+++ src/PISO_arr.vhd
@@ -32,35 +32,35 @@ begin
 		
 		variable readPerformed: boolean := false;
 		variable writePerformed: boolean := false;
+		variable resetPerformed: boolean := false;
 		
 		-- We use a buffer so the output is not instantly affected when memory is changed
 		variable outputBuffer: signed(dataWidth-1 downto 0);
 	begin
 		if rising_edge(clk) then
 			if n_rst = '0' then -- Check for reset triggered
-				read_ready <= '0';
-				write_ready <= '1';
-				-- Reset outputIndex
-				if outputFromStart then
-					outputIndex := 0;
-				else
-					outputIndex := arrSize-1;
-				end if;
+				resetPerformed := true;
+				
+				mem := (others => to_signed(0, dataWidth));
 				-- Reset state variables
 				writePerformed := false;
 				readPerformed  := false;
-				outputBuffer   := to_signed(0, dataWidth);
-				mem := (others => to_signed(0, dataWidth));
+				
 			else
+				resetPerformed := false;
 				-- Data input
 				if write_en = '1' then
 					mem := parallel_in;
 					writePerformed := true;
+				else
+					writePerformed := false;
 				end if;
 				
 				-- Data output
 				if read_en = '1' then
 					readPerformed := true;
+				else
+					readPerformed := false;
 				end if;	
 			end if;
 		-- Changes to outputs are only made on falling edge
@@ -82,7 +82,6 @@ begin
 						write_ready <= '1';
 					end if;
 				end if;
-				readPerformed := false;
 			end if;
 			
 			if writePerformed then
@@ -92,10 +91,22 @@ begin
 				else
 					outputIndex := arrSize-1;
 				end if;
-				writePerformed := false;
 				read_ready <= '1';
 				write_ready <= '0';
 			end if;
+			
+			if resetPerformed then
+				-- Reset outputIndex
+				if outputFromStart then
+					outputIndex := 0;
+				else
+					outputIndex := arrSize-1;
+				end if;
+				outputBuffer   := to_signed(0, dataWidth);
+				read_ready <= '0';
+				write_ready <= '1';
+			end if;
+			
 			
 			outputBuffer := mem(outputIndex); -- update output buffer
 			serial_out <= outputBuffer;
